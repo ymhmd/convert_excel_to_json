0f68730194e58bc4180dbd081f7851a4cbebfc27
Yasin Mohamed <Yasin Mohamed>
2017-03-10 15:57:56 +0200

    -

--- push_code_to_GitHub.bat
+++ push_code_to_GitHub.bat
@@ -1,8 +1,10 @@
 SET URL="https://github.com/ymhmd/convert_excel_to_json.git"
 git reset HEAD .
+git status
 git init
 git add .
 %git commit -m "-"
 git remote add origin "%URL%" 
 git push -u origin master
+git status
 pause
